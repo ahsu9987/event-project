c76bc1c18436fc7dcc1d38d9dbc90503fc85af63
ahsu9987 <ahsanansari75862@gmail.com>
2023-08-27 17:41:28 +0530

    update java code

--- function.js
+++ function.js
@@ -84,12 +84,88 @@
 
 //---------------------------------------------------------------------------------------------------------------------//
 
-let hello = "hii"; 
-hello = "hey hii"; //global
-let times = 4 ;
+// let hello = "hii"; 
+// hello = "hey hii"; //global
+// let times = 4 ;
 
-if (times > 3){
-    let hello1 = "hi bro" + hello  ; 
-    console.log(hello1);
-}
-console.log(hello);
+// if (times > 3){
+//     let hello1 = "hi bro" + hello  ; 
+//     console.log(hello1);
+// }
+// console.log(hello);
+// //---------------------------------------------------------------------------------------------------------------------//
+// console.log('1-swapping-variables');
+
+// let var1 = "one";
+// let var2 = "two";
+
+// console.log('before swap in var1 :',var1);
+// console.log('before swap in var2 :',var2);
+
+
+// let tempvar = var1;
+// var1 = var2;
+// var2 = tempvar;
+
+// console.log('After swap in var1', var1);
+// console.log('After swap in var2', var2);
+
+//---------------------------------------------------------------------------------------------------------------------//
+
+// function findMaxNumberLong(num1 , num2){
+// // 1. long syntax
+// if(num1>num2){
+//     return num1
+// } 
+// else if (num1==num2){                        //...numbers are equal
+//     alert("Number are equal")
+// }
+// else
+// {
+//     return num2
+// }
+// }
+
+// function findMaxNumberShort(num1 , num2){
+//     // 2. short syntax 
+//     if(num1 > num2) return num1;
+//     else if (num1==num2){                        //...numbers are equal
+//         alert("Number are equal")
+//     }
+//     else return num2;  
+// }
+
+// function findMaxNumberternaryShort(num1 , num2){
+//  // 3.ternary short syntax
+// return (num1 > num2) ? num1 : num2;
+// }
+
+// let checkMax1 = findMaxNumberLong(10,10);                            //...numbers are equal
+// console.log('Max Number from Long Syntax :', checkMax1);
+
+// let checkMax2 = findMaxNumberShort(25,25);                        //...numbers are equal
+// console.log('Max Number from Long Syntax :', checkMax2);
+
+// let checkMax3 = findMaxNumberternaryShort(100,100);
+// console.log('Max Number from Long Syntax :', checkMax3);
+
+//---------------------------------------------------------------------------------------------------------------------//
+
+// function isLandscapePortrait(width , height){
+//     if(width>height){
+//         return "Landscape";
+//     }
+//     else{
+//         return "portrait";
+//     }
+// }
+
+// // function isportrait(height, width){
+// //     return (height > width);
+// // }
+
+// let checkWidthHeight1 = isLandscapePortrait(1024, 400);
+// console.log('Landscape:',checkWidthHeight1);
+
+// let checkWidthHeight2 = isLandscapePortrait(300,900 );
+// console.log('portrait:',checkWidthHeight2);
